0448d1e199c0b8f0b9bd622e007656c24db04f24
Kyle Maysey <KyleMaysey@kyles-mac-mini.home>
2013-03-15 20:03:58 -0700

    Initial Commit

---
+++ DiscountCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DiscountCalculator
+//
+//  Created by Kyle Maysey on 3/15/13.
+//  Copyright (c) 2013 Kyle Maysey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiscountCalculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiscountCalculator
+//
+//  Created by Kyle Maysey on 3/15/13.
+//  Copyright (c) 2013 Kyle Maysey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
